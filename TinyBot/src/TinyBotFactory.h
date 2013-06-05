86308d00140602dd2f96ddbf07f3b2e9a3b51f48
Juian Löhr <julian.loehr@fh-heidelberg.de>
2013-06-06 01:55:21 +0200

    CHANGED added copyright and license note to source files; ADDED license and readme files

--- TinyBot/src/TinyBotFactory.h
+++ TinyBot/src/TinyBotFactory.h
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright (C) 2013 Julian Löhr
+ * All rights reserved.
+ *
+ * This file is part of TinyBot, an IRC Bot.
+ * TinyBot is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * To read the full license, please check the LICENSE file.
+ *
+*/
 #pragma once
 
 #include <cstring>
