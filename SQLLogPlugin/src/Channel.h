46504bf9cfc8fa38993a0b743597acd830e36a46
Juian Löhr <julian.loehr@fh-heidelberg.de>
2013-06-06 14:16:07 +0200

    ADDED License and Readme files; CHANGED added copyright and license note to sourcefiles

--- SQLLogPlugin/src/Channel.h
+++ SQLLogPlugin/src/Channel.h
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright (C) 2013 Julian Löhr
+ * All rights reserved.
+ *
+ * This file is part of SQLLogPlugin, a Logging Plugin for TinyBot.
+ * SQLLogPlugin is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * To read the full license, please check the LICENSE file.
+ *
+*/
 #pragma once
 
 #include <string>
